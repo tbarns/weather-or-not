3c54d5bb5535c9d86fb2d3eb7a6ca815df0afc80
Timothy Barnaby <tbarnaby1@gmail.com>
2022-09-18 16:47:35 -0500

    added structre to HTML to createa an aside that houses the search history, psuedo coded JS to plan out how to make the history remain in local storage and how to create buttons that have the cerac history cities linked. also added some CSS to make the search button more user freindly

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 //connects to open weather one call API
 
 var today = moment();
-
+var searchBox = $(aside)
 
 
 // displays time and date 
@@ -9,6 +9,8 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 //weather dashboad that contains form inputs
 //search function by city
+// HINT
+// Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
 //presents city's current and future conditions
 //presents city's name date and an icon representing weather conditions, temp, humidity, wind speed and uv index
 //when viewong the UV index im presented with a color idicating either favorabel, moderate, or severe
@@ -19,3 +21,15 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 //local storage that contains city serach history
 //when i click a city in history i am displayed current and future conditions
 
+
+function historyBtnUpdater(){
+    //this needs to fun a foreach everytime the search button is clicked the value of the inpit needs to be added to the text content of a new button and displayed below.  there needs to be a stop default aspect that allows the history buttins to persist.
+}
+
+$(".saveBtn").on("click", function () {
+
+    var city = $(searchBox).siblings("input").val()
+    //this is the data from weather API update when you understand this portion better
+    var data = $(searchBox).siblings("input").val()
+    localStorage.setItem(city, data)
+})
