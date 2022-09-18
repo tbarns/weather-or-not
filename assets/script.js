f7058bb6e249eced1b4e632dc10370f131123932
Timothy Barnaby <tbarnaby1@gmail.com>
2022-09-18 16:19:56 -0500

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//connects to open weather one call API
+
+var today = moment();
+
+
+
+// displays time and date 
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+//weather dashboad that contains form inputs
+//search function by city
+//presents city's current and future conditions
+//presents city's name date and an icon representing weather conditions, temp, humidity, wind speed and uv index
+//when viewong the UV index im presented with a color idicating either favorabel, moderate, or severe
+
+//furture: 5 day forecast displays date, an icon repping the weather conditions, temp, wind speed, and humidity
+
+
+//local storage that contains city serach history
+//when i click a city in history i am displayed current and future conditions
+
