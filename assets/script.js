3c5e968c616177402bdd651a850bbfd3008a11f9
Timothy Barnaby <tbarnaby1@gmail.com>
2023-05-04 12:51:19 -0500

    working on pagination and styling

--- assets/script.js
+++ assets/script.js
@@ -1,90 +1,62 @@
-// Define constants and variables at the top, and use let or const instead of var
 const apiKey = "f134c88b914b12f6422fd757a1b6307c";
 let city;
-// Helper function to capitalize the first letter of each word
+
 const capitalizeWords = str => {
     return str.replace(/\w\S*/g, word => {
         return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
     });
 };
 
-
-
 const getCity = () => {
     city = capitalizeWords($("input").val());
     const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    appendHistory(city);
-
+    updateLocalStorage(city);
+    console.log('click')
     fetch(queryURL)
         .then(response => response.json())
         .then(data => {
-            const { lon, lat } = data.coord; // use destructuring to avoid repetitive data.coord
+            const { lon, lat } = data.coord;
             fiveDay(lat, lon);
         });
 };
 
-
 const appendHistory = city => {
     const history = $("<div>").text(city).addClass("card");
     $("#history").append(history);
+};
 
-    // Update local storage with each new search
+const updateLocalStorage = city => {
     const storedHistory = JSON.parse(localStorage.getItem("history")) || [];
-    // Check if the city is already in the stored history
     if (storedHistory.indexOf(city) === -1) {
-        const history = $("<div>").text(city).addClass("card");
-        $("#history").append(history);
-        updateLocalStorage(city);
+        storedHistory.push(city);
+        localStorage.setItem("history", JSON.stringify(storedHistory));
+        renderHistory(1);
     }
 };
 
-// New function to update local storage with a new city
-const updateLocalStorage = city => {
-    const storedHistory = JSON.parse(localStorage.getItem("history")) || [];
-    storedHistory.push(city);
-    localStorage.setItem("history", JSON.stringify(storedHistory));
-};
-
-
 const clearHistory = () => {
-    // Empty the history section
-    $("#history").empty();
-
-    // Add the input, search button and clear button back to the history section
-    const input = $('<input>').addClass('col-md-10').attr('placeholder', 'Type your next location here');
-    const searchBtn = $('<button>').addClass('col-md-10').attr('id', 'searchBtn').text('SEARCH');
-    const clearBtn = $('<button>').addClass('col-md-10').attr('id', 'clearBtn').text('CLEAR HISTORY');
-    $("#history").append(input, '<br><br>', searchBtn, '<br>', clearBtn);
-
-    // Clear the local storage
     localStorage.removeItem("history");
-
-    // Re-assign the event listeners
-    $("#searchBtn").on("click", getCity);
-    $("#clearBtn").on("click", clearHistory);
-    $("#history").on("click", ".card", function () {
-        const city = $(this).text();
-        $("input").val(city);
-        getCity();
-    });
+    renderHistory(1);
+    console.log('clearedHistory')
 };
 
-// Add event listener to the "CLEAR HISTORY" button
 $("#clearBtn").on("click", clearHistory);
-
 
 const fiveDay = (lat, lon) => {
     city = $("input").val();
-
+console.log("fivedayfunction")
     fetch(
         `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
     )
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-
-            // Use an array and loop to handle future days
-            const days = ["Today", "Tomorrow", moment().add(2, "days").format("dddd"), moment().add(3, "days").format("dddd"), moment().add(4, "days").format("dddd")];
+            const days = [
+                "Today",
+                "Tomorrow",
+                moment().add(2, "days").format("dddd"),
+                moment().add(3, "days").format("dddd"),
+                moment().add(4, "days").format("dddd")
+            ];
             days.forEach((day, index) => {
                 const temp = data.list[index * 8].main.temp;
                 const humidity = data.list[index * 8].main.humidity;
@@ -98,32 +70,20 @@ const fiveDay = (lat, lon) => {
                 const dayWind = $("<p>").append(`Windspeed: ${windSpeed} mph`);
                 const iconImage = $("<img>").attr("src", iconUrl);
 
-                $(`#day${index + 1}`).empty(); // Use template literals and string interpolation to simplify code
+                $(`#day${index + 1}`).empty();
                 $(`#day${index + 1}`).append(dayCity, dayTemp, dayWind, dayHumidity, iconImage);
             });
         });
 };
 
-$("#searchBtn").on("click", () => {
+$("#searchBtn").on("click", (event) => {
+    console.log('searchClick')
+    event.preventDefault();
     getCity();
-    updateLocalStorage(capitalizeWords($("input").val()));
 });
-const title = $("<p>").text("5-day weather forecast");
 
-// Use moment.js to format the current date
 $("#currentDay").text(moment().format("dddd, MMM Do, YYYY"));
-$("#currentDay").append(title);
-
-// Store search history in local storage and retrieve it on page load
-const history = JSON.parse(localStorage.getItem("history")) || [];
-history.forEach(city => appendHistory(city));
-$("#history").on("click", ".card", function () {
-    const city = capitalizeWords($(this).text());
-    $("input").val(city);
-    getCity();
-});
-
-
+$("#currentDay").append($("<p>").text("5-day weather forecast"));
 
 
 
@@ -133,41 +93,55 @@ const renderHistory = currentPage => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const storedHistory = JSON.parse(localStorage.getItem("history")) || [];
-  
+    console.log('renderHistory')
     // Clear the history section
     $("#history").empty();
-  
+
     // Add the input, search button, and clear button back to the history section
     const input = $('<input>').addClass('col-md-10').attr('placeholder', 'Type your next location here');
     const searchBtn = $('<button>').addClass('col-md-10').attr('id', 'searchBtn').text('SEARCH');
     const clearBtn = $('<button>').addClass('col-md-10').attr('id', 'clearBtn').text('CLEAR HISTORY');
     $("#history").append(input, '<br><br>', searchBtn, '<br>', clearBtn);
-  
 
-// Add history cards for the current page
-storedHistory.slice(startIndex, endIndex).forEach(city => appendHistory(city));
+    $("#searchBtn").on("click", (event) => {
+        console.log('searchClick');
+        event.preventDefault();
+        getCity();
+    });
+
+    $("#clearBtn").on("click", clearHistory);
+
+
+    // Add history cards for the current page
+    storedHistory.slice(startIndex, endIndex).forEach(city => appendHistory(city));
 
 
 
 
-// Render the pagination
-const totalPages = Math.ceil(storedHistory.length / itemsPerPage);
-const pagination = $('<nav>').attr('aria-label', 'History pagination');
-const paginationUl = $('<ul>').addClass('pagination');
-for (let i = 1; i <= totalPages; i++) {
-    const paginationLi = $('<li>').addClass('page-item').toggleClass('active', i === currentPage);
-    const paginationLink = $('<a>').addClass('page-link').attr('href', '#').text(i);
-    paginationLi.append(paginationLink);
-    paginationUl.append(paginationLi);
-}
-$("#history").append(pagination);
-$("#history").on("click", ".page-link", function (event) {
-    event.preventDefault();
-    const newPage = parseInt($(this).text());
-    renderHistory(newPage);
-});
+    // Render the pagination
+    const totalPages = Math.ceil(storedHistory.length / itemsPerPage);
+    const pagination = $('<nav>').attr('aria-label', 'History pagination');
+    const paginationUl = $('<ul>').addClass('pagination');
+    for (let i = 1; i <= totalPages; i++) {
+        const paginationLi = $('<li>').addClass('page-item').toggleClass('active', i === currentPage);
+        const paginationLink = $('<a>').addClass('page-link').attr('href', '#').text(i);
+        paginationLi.append(paginationLink);
+        paginationUl.append(paginationLi);
+    }
+    pagination.append(paginationUl);
+    $("#history").append(pagination);
+    $("#history").on("click", ".page-link", function (event) {
+        event.preventDefault();
+        const newPage = parseInt($(this).text());
+        renderHistory(newPage);
+    });
 };
-
 
 // Call renderHistory on page load
 renderHistory(1);
+
+$("#history").on("click", ".card", function () {
+    const city = capitalizeWords($(this).text());
+    $("input").val(city);
+    getCity();
+});
